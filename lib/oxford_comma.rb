7c4f0f79aa8a1dc2f342a27f122b443f52da92f4
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-06-03 19:16:18 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,8 +4,7 @@ def oxford_comma(array)
   else if array.size == 2 
     return array.join(" and ")
   else 
-    
-    array[-1] = array[-1]
+    array[-1] = "and " + array[-1]
     array.join(", ")
   end
 end
@@ -13,6 +12,6 @@ end
 
 # it "adds commas plus a final 'and' when given a 3-element array" do
 #     expect(oxford_comma(["kiwi", "durian", "starfruit"])).to eq("kiwi, durian, and starfruit")
-"kiwi, durian, starfruit"
-"kiwi, durian, and starfruit"
+# "kiwi, durian, starfruit"
+# "kiwi, durian, and starfruit"
 #   end
